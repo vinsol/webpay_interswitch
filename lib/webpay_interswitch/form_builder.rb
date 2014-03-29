5cc2a7f4cde2f99fafea3d5f9da945a3aaa3458c
shubham gupta <shubham.gupta@vinsol.com>
2014-03-29 17:09:36 +0530

    Add support for providing html attributes to the submit button

--- lib/webpay_interswitch/form_builder.rb
+++ lib/webpay_interswitch/form_builder.rb
@@ -3,19 +3,23 @@ module WebpayInterswitch
 
     include WebpayInterswitch::Core
 
-    def initialize(txn_ref, amount)
+    def initialize(txn_ref, amount, html_options)
       @txn_ref = txn_ref
       @amount = amount
+      @html_options = html_options
+      sanitize_options
     end
 
-    def generate_webpay_form(submit_button_text)
+    def generate_webpay_form
       form_html = "<form action=#{ WebpayInterswitch::Gateway.url } method=post>"
 
       form_html += generate_webpay_elements
 
       form_html += generate_transaction_data_elements
 
-      form_html += generate_input_field('commit', submit_button_text, 'submit')
+      # This generates the submit button alongwith the @submit_button_text
+      # If submit_button_text is not provided, it defaults to 'Make Payment'
+      form_html += generate_input_field('commit', @submit_button_text, 'submit', @html_options)
 
       form_html += '</form>'
 
@@ -41,8 +45,10 @@ module WebpayInterswitch
         txn_elem_html += generate_input_field('hash', sha_hash(string_for_hash_param))
       end
 
-      def generate_input_field(name, value, type = 'hidden')
-        "<input type=#{ type } name=#{ name } value='#{ value }'>"
+      # Generates the input tag alongwith the provided name, value, type and html_options if any.
+      def generate_input_field(name, value, type = 'hidden', html_options={})
+        html_string = string_for_html_options(html_options)
+        "<input type=#{ type } name=#{ name } value='#{ value }' #{html_string}>"
       end
 
       ## Returns a string that is used to compute the sha hash for POST request on webpay.
@@ -53,6 +59,22 @@ module WebpayInterswitch
         @amount.to_s +
         WebpayInterswitch::Gateway.site_redirect_url +
         WebpayInterswitch::Gateway.mac_key
+      end
+
+      ## Returns a string from html_options that is embedded into the submit button.
+      ## e.g: html_options = {id: 'hi', class: 'c1 c2'}
+      ## output: "id='hi' class='c1 c2'"
+      def string_for_html_options(html_options)
+        html_string = ''
+        html_options.each do |attr, val|
+          html_string << " #{attr}='#{val}'"
+        end
+        html_string
+      end
+
+      def sanitize_options
+        @html_options.symbolize_keys!
+        @submit_button_text = @html_options.delete(:submit_button_text) || 'Make Payment'
       end
 
   end
--- lib/webpay_interswitch/view_helper.rb
+++ lib/webpay_interswitch/view_helper.rb
@@ -1,10 +1,10 @@
 module WebpayInterswitch
   module ViewHelper
 
-    def form_for_webpay(txn_ref, amount, submit_button_text='Make Payment')
-      form_builder = WebpayInterswitch::FormBuilder.new(txn_ref, amount)
+    def form_for_webpay(txn_ref, amount, html_options={})
+      form_builder = WebpayInterswitch::FormBuilder.new(txn_ref, amount, html_options)
       WebpayInterswitch::Gateway.new.validate!
-      form_builder.generate_webpay_form(submit_button_text).html_safe if form_builder.valid?
+      form_builder.generate_webpay_form.html_safe if form_builder.valid?
     end
 
   end
