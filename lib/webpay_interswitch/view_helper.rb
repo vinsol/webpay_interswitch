5d24c98badd9f560edf1de530cf7604fdac79adf
aishmita-aggarwal <aishmita@vinsol.com>
2015-07-21 16:07:03 +0530

    Add optional_parameters to form_builder for webpay optional_params

--- lib/webpay_interswitch/view_helper.rb
+++ lib/webpay_interswitch/view_helper.rb
@@ -1,8 +1,8 @@
 module WebpayInterswitch
   module ViewHelper
 
-    def form_for_webpay(txn_ref, amount, html_options={})
-      form_builder = WebpayInterswitch::FormBuilder.new(txn_ref, amount, html_options)
+    def form_for_webpay(txn_ref, amount, html_options={}, optional_parameters={})
+      form_builder = WebpayInterswitch::FormBuilder.new(txn_ref, amount, html_options, optional_parameters)
       WebpayInterswitch::Gateway.new.validate!
       form_builder.generate_webpay_form.html_safe if form_builder.valid?
     end
